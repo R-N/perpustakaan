9a0aee3bba7c3fc7a8327cc5e1523afabc88a8d3
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2019-12-17 13:57:34 +0700

    rename project

--- src/perpustakaan/Perpustakaan.java
+++ src/perpustakaan/Perpustakaan.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package perpustakaan2;
+package perpustakaan;
 
 /**
  *
  * @author LENOVO
  */
-public class Perpustakaan2 {
+public class Perpustakaan {
 
     /**
      * @param args the command line arguments
